7b84c18d8f81cb70455b92e610ab07a0263513da
Redstomite <rahul.r.prabhu@gmail.com>
2018-08-12 12:27:12 +0530

    Added logout, search

--- api.py
+++ api.py
@@ -1,23 +1,57 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'random string'
 
 @app.route("/home")
 def index():
-	return render_template('welcome.html')
+	if("username" in session.keys()):
+		return render_template('welcome.html', login="True")
+	else:
+		return render_template('welcome.html', login="False")
+
+
+
+@app.route('/logout')
+def logout():
+   session.pop('username', None)
+   return redirect(url_for('index'))
+
+
 
 @app.route("/products")
 def products():
-	username = request.args["username"]
-	return render_template('products.html', username=username)
+	if("username" in session.keys()):
+		return render_template('products.html')
+	else:
+		return redirect(url_for('index'))
+
+
+@app.route("/search")
+def search():
+	if("username" in session.keys()):
+		search_query = request.args.get("search")
+		print (search_query)
+		products = ["apple", "banana", "tomato"]
+		if search_query in products:
+			return render_template('search_results.html', products=search_query)
+	else:
+		return redirect(url_for('index'))
 	
+	
+
+
 @app.route("/login", methods=['POST'])
 def login():
 	username = request.form["username"]
 	if(username == "Rahul" and request.form["password"] == "rahulp"):
 		print("Login succesful, showing you the project page")
-		return redirect(url_for('products', username=username))
+		session['username'] = username
+		return redirect(url_for('index'))
 	else:
 		return render_template('error.html')
+
+
+
 if(__name__ == "__main__"):
 	app.run(port=4000)
